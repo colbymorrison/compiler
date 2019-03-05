95782b30e308f60c94740ace8b74e88b3e33af90
Colby Morrison <colbyamorrison@gmail.com>
2019-03-05 17:22:37 -0500

    Last commit before SymbolTable & Semantic Actions 1.

--- src/test/java/compiler/Test/ParserTest.java
+++ src/test/java/compiler/Test/ParserTest.java
@@ -1,37 +1,37 @@
-package compiler.Test;
-
-import compiler.Exception.ParserError;
-import compiler.Lexer.Lexer;
-import compiler.Parser.Parser;
-import org.junit.jupiter.api.Test;
-
-import java.io.File;
-
-class ParserTest {
-    /**
-     * Tests the source files first for parser specific test files
-     * Then for general test files.
-     *
-     * @throws
-     */
-    @Test
-    void testFiles() throws ParserError {
-        String resPath = "src/test/resources/";
-        testParser(new File(resPath + "Parser"));
-//        testParser(new File(resPath + "Code"));
-//        testParser(new File(resPath + "Now"));
-    }
-
-
-    private void testParser(File dir) throws ParserError {
-        for (File f : dir.listFiles()) {
-//            if(f.getName().equals("func.vas")) {
-            System.out.println(f.getAbsolutePath());
-            Lexer lexer = new Lexer(f.getAbsolutePath());
-            Parser parser = new Parser(lexer, true);
-            parser.parse();
-//            }
-        }
-    }
-
-}
+//package compiler.Test;
+//
+//import compiler.Exception.ParserError;
+//import compiler.Lexer.Lexer;
+//import compiler.Parser.Parser;
+//import org.junit.jupiter.api.Test;
+//
+//import java.io.File;
+//
+//class ParserTest {
+//    /**
+//     * Tests the source files first for parser specific test files
+//     * Then for general test files.
+//     *
+//     * @throws
+//     */
+//    @Test
+//    void testFiles() throws ParserError {
+//        String resPath = "src/test/resources/";
+//        testParser(new File(resPath + "Parser"));
+////        testParser(new File(resPath + "Code"));
+////        testParser(new File(resPath + "Now"));
+//    }
+//
+//
+//    private void testParser(File dir) throws ParserError {
+//        for (File f : dir.listFiles()) {
+////            if(f.getName().equals("func.vas")) {
+//            System.out.println(f.getAbsolutePath());
+//            Lexer lexer = new Lexer(f.getAbsolutePath());
+//            Parser parser = new Parser(lexer, true);
+//            parser.parse();
+////            }
+//        }
+//    }
+//
+//}
